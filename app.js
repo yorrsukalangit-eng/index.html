bb1e37e16fe147c7c24d2691e8764fc840c6145e
yorrsukalangit-eng <yorrsukalangit@gmail.com>
2026-02-12 18:25:17 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+const btn = document.getElementById("loveBtn");
+const screen = document.getElementById("startScreen");
+const audio = document.getElementById("bgm");
+
+btn.addEventListener("click", () => {
+  // Hilangkan layar tombol
+  screen.style.display = "none";
+
+  // Jalankan animasi bunga
+  document.body.classList.remove("not-loaded");
+
+  // Mainkan musik
+  audio.play();
+});
